75c7bb7fdd2ee80343c17abbde4a44b17516254a
ANISH SARANATH <shuba.aca@gmail.com>
2025-04-07 13:57:19 +0530

    home page!

---
+++ Frontend/Home page/script1.js
@@ -0,0 +1,7 @@
+document.querySelector('.signup-btn').addEventListener('click', () => {
+  window.location.href = 'home.html';
+});
+
+document.querySelector('.google-btn').addEventListener('click', () => {
+  alert("Google Sign-Up Coming Soon!");
+});
